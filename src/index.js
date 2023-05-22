8cd2ecc9d891759171be083382104e3a392f1d27
Tamara Zdrnja <tamarazdrnja53@gmail.com>
2023-05-22 11:46:53 +0200

    new project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+import App from "./App";
+//import "./css/styles.css";
+//import "./scss/main.scss";
+
+var root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App title="Title from index" content="Content from index" number={10} />);
