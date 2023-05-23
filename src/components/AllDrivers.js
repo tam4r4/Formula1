c29c75880758d95c328c74dce91c71d770f8d7be
Dragana63 <d.skondric63@gmail.com>
2023-05-23 11:43:01 +0200

    details

--- src/components/AllDrivers.js
+++ src/components/AllDrivers.js
@@ -3,42 +3,38 @@ import React from "react";
 //import Loader from "./Loader";
 
 export default class AllDrivers extends React.Component {
+  state = {
+    drivers: [],
+    // loading: true
+  };
 
-    state = {
-        drivers: [],
-        // loading: true
-    }
+  componentDidMount() {
+    this.getDrivers();
+  }
 
-    componentDidMount() {
+  getDrivers = async () => {
+    const url = "https://ergast.com/api/f1/2013/driverStandings.json";
 
-        this.getDrivers();
-    }
+    const response = await axios.get(url);
+    let podaci =
+      response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]
+        .Driver;
+    console.log(podaci);
+    this.setState({
+      drivers: podaci,
+      //  loading: false
+    });
+  };
 
-    getDrivers = async () => {
-        const url = "https://ergast.com/api/f1/2013/driverStandings.json";
-
-        const response = await axios.get(url);
-        let podaci = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver;
-        console.log(podaci);
-        this.setState({
-            drivers: podaci,
-            //  loading: false
-        });
-
-    }
-
-    render() {
-
-
-        return (
-            <div>
-                {this.state.drivers.map(x =>
-                    <div>
-                        <p> Nesto:</p>
-                    </div>
-                )}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {this.state.drivers.map((x) => (
+          <div>
+            <p> Nesto:</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
-
