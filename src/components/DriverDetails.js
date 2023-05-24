d82d597ead75c06db35257899993ac6514d9e252
Dragana63 <d.skondric63@gmail.com>
2023-05-24 09:10:41 +0200

    details

--- src/components/DriverDetails.js
+++ src/components/DriverDetails.js
@@ -14,27 +14,29 @@ export default class DriverDetails extends React.Component {
   getDriverDetails = async () => {
     const id = this.props.match.params.name;
     const url = `https://ergast.com/api/f1/2013/drivers/${id}/driverStandings.json`;
+    console.log(url);
     const response = await axios.get(url);
     console.log(
       response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]
     );
-    console.log(this.state.driverDetails.Driver.driverId);
     this.setState({
       driverDetails:
         response.data.MRData.StandingsTable.StandingsLists[0]
           .DriverStandings[0],
 
-      //loading: false
+      loading: false,
     });
+    //console.log(this.state.driverDetails.Driver.driverId);
   };
   render() {
     return (
       <div>
-        <p>Name:</p>
-        <p>Country:</p>
-        <p>Team:</p>
-        <p>Birth</p>
-        <p>Biography</p>
+        <p>Name:{this.state.driverDetails.Driver?.givenName}</p>
+        <p>Family Name:{this.state.driverDetails.Driver?.familyName}</p>
+        <p>Country:{this.state.driverDetails.Driver?.nationality}</p>
+        <p>Team:{this.state.driverDetails.Constructors?.name}</p>
+        <p>Birth:{this.state.driverDetails.Driver?.dateOfBirth}</p>
+        <p>Biography:{this.state.driverDetails.Driver?.url}</p>
       </div>
     );
   }
