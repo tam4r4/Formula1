54872b9c75f60608222803156a56c00c5220ce69
Tamara Zdrnja <tamarazdrnja53@gmail.com>
2023-05-25 10:58:05 +0200

    import css

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 import App from "./App";
-//import "./css/styles.css";
-//import "./scss/main.scss";
+import "./css/styles.css";
 
 var root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<App title="Title from index" content="Content from index" number={10} />);
