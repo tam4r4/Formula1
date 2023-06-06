7f060228e8f1f460f8b6a118bced80659c6de32a
Tamara Zdrnja <tamarazdrnja53@gmail.com>
2023-06-06 16:27:41 +0200

    img path

--- src/App.js
+++ src/App.js
@@ -19,26 +19,26 @@ export default class App extends React.Component {
           <YearProvider>
             <nav className="nav-icons-container">
 
-              <NavLink to="/"><img src="../img/f1_logo.png" className="logo-icon" /></NavLink>
+              <NavLink to="/"><img src="./../img/f1_logo.png" className="logo-icon" /></NavLink>
 
               <div className="drivers-link">
                 <NavLink to="/drivers" exact activeClassName="active-link">
                   <p>Drivers</p>
-                  <img src="../img/drivers.png" alt="Drivers" className="icon-drivers" />
+                  <img src="./../img/drivers.png" alt="Drivers" className="icon-drivers" />
                 </NavLink>
               </div>
 
               <div className="teams-link">
                 <NavLink to="/teams" exact activeClassName="active-link">
                   <p>Teams</p>
-                  <img src="../img/teams.png" alt="Teams" className="icon-teams" />
+                  <img src="./../img/teams.png" alt="Teams" className="icon-teams" />
                 </NavLink>
               </div>
 
               <div className="races-link">
                 <NavLink to="/races" exact activeClassName="active-link">
                   <p>Races</p>
-                  <img src="../img/races.png" alt="Races" className="icon-races" />
+                  <img src="./../img/races.png" alt="Races" className="icon-races" />
                 </NavLink>
               </div>
             </nav>
